e4564e64ca582d233f9fda657e51524f06b12eeb
Your Name <you@example.com>
2021-12-21 23:48:20 +0900

    UI Design with Tailwind CSS

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 // ts-check
 
 const Koa = require('koa')
+const Pug = require('koa-pug')
+const path = require('path')
 
 const app = new Koa()
 
+// eslint-disable-next-line no-new
+new Pug({
+  viewPath: path.resolve(__dirname, './views'),
+  app,
+})
+
 app.use(async (ctx) => {
   ctx.body = 'Hello World!'
+  await ctx.render('main')
 })
 
 app.listen(5000)
